e6d4422284678e12bf98d5100c91ee6074322085
Julian Setiawan <julian.setiawan@solace.com>
2020-02-20 07:43:40 -0500

    Add proxying by service name

--- lambda_function.py
+++ lambda_function.py
@@ -13,11 +13,26 @@ def lambda_handler(event, context):
     http_method = event['httpMethod']
     path = event['path'].split('/')
 
-    service_id = path[1]
+    service_query = path[1]
     passthrough_path = '/'.join(path[2:])
 
-    service = requests.get(f'https://api.solace.cloud/api/v0/services/{service_id}', headers={'Authorization': auth}).json()
-    soladmin = [item for item in service['data']['managementProtocols'] if item['name'] == 'SolAdmin'][0]
+    service_by_name = requests.get(f'https://api.solace.cloud/api/v0/services?name={service_query}', headers={'Authorization': auth}).json()
+
+    if len(service_by_name['data']) > 0:
+        service_id = service_by_name['data'][0]['serviceId']
+    else:
+        service_id = service_query
+
+    service_by_id = requests.get(f'https://api.solace.cloud/api/v0/services/{service_id}', headers={'Authorization': auth})
+    if service_by_id.status_code == 404:
+        return {
+            'statusCode': 404,
+            'body': 'Service not found'
+        }
+    else:
+        service = service_by_id.json()['data']
+
+    soladmin = [item for item in service['managementProtocols'] if item['name'] == 'SolAdmin'][0]
     username = soladmin['username']
     password = soladmin['password']
     url = [item for item in soladmin['endPoints'] if item['name'] == 'Secured Management'][0]['uris'][0]
